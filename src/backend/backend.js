64433ad4d31637bc5af8e2a8a04ccf582aa18f07
derek ney <deek311@gmail.com>
2022-12-10 19:01:20 -0700

    added mongo

--- src/backend/backend.js
+++ src/backend/backend.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
+const mongodb = require('mongodb');
 
 app.use(bodyParser.json());
 app.use(cors());
